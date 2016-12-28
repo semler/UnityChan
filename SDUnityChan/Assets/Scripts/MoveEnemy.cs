100a8f40d63150614bcc48353f7ffb8d612fbd11
fan-c-yu <c_yu@fancs.com>
2016-12-28 14:51:17 +0900

    s

---
+++ SDUnityChan/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour {
+
+	private CharacterController eCon;
+	private Animator animator;
+	private Vector3 destination;	//目的地
+	private Vector3 randDestination;	//目的地
+	private Vector3 velocity;        //速度×方向
+	private Vector3 direction;       //移動方向
+	private float waitTime;       
+	private float currentTime;
+	private GameObject unityChan;
+							// キャラにアタッチされるアニメーターへの参照
+	private AnimatorStateInfo stateInfo;
+
+	// Use this for initialization
+	void Start () {
+		eCon = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+		velocity = Vector3.zero;
+		unityChan = GameObject.Find("SD_unitychan_humanoid");
+		waitTime = 2.0f;
+		currentTime = 0.0f;
+		randDestination = RandDestination();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		stateInfo = animator.GetCurrentAnimatorStateInfo (0);
+		destination = new Vector3(unityChan.transform.position.x, unityChan.transform.position.y, unityChan.transform.position.z);
+
+		if (Vector3.Distance (destination, transform.position) > 10) {
+			animator.SetBool ("Attack", false);
+
+			if (Vector3.Distance (randDestination, transform.position) < 0.5) {
+				currentTime += Time.deltaTime;
+				if (currentTime > waitTime) {
+					animator.SetBool ("Stop", false);
+					randDestination = RandDestination ();
+					currentTime = 0.0f;
+					Walk (randDestination, 0.5f);
+				} else {
+					animator.SetBool ("Stop", true);
+				}
+			} else {
+				animator.SetBool ("Stop", false);
+				Walk (randDestination, 0.5f);
+			}
+		} else if (Vector3.Distance (destination, transform.position) < 1.5) {
+			animator.SetBool ("Attack", true);
+			animator.SetBool ("Stop", false);
+
+			direction = (destination - transform.position).normalized;
+			transform.LookAt (new Vector3 (destination.x, transform.position.y, destination.z));
+
+			if (stateInfo.IsName ("Attack")) {
+				unityChan.SendMessage ("StartAttacked");
+			}
+		} else {
+			animator.SetBool ("Attack", false);
+			animator.SetBool ("Stop", false);
+
+			Walk (destination, 1.0f);
+		}
+	}
+
+	private void Walk (Vector3 destination, float speed) {
+		if (eCon.isGrounded) {
+			velocity = Vector3.zero;
+			direction = (destination - transform.position).normalized;
+			transform.LookAt (new Vector3 (destination.x, transform.position.y, destination.z));
+			velocity = direction * speed;
+		}
+		velocity.y += Physics.gravity.y * Time.deltaTime;
+		eCon.Move (velocity * Time.deltaTime);
+	}
+
+	private Vector3 RandDestination () {
+		Vector3 random = Random.insideUnitCircle * 10;
+		return transform.position + new Vector3(random.x, 0, random.z);
+	}
+}
---
+++ SDUnityChan/Assets/Yurowm/Demo/Scripts/Actions.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Animator))]
+public class Actions : MonoBehaviour {
+
+	private Animator animator;
+	private CapsuleCollider col;
+	private Vector3 velocity;
+
+	const int countOfDamageAnimations = 3;
+	int lastDamageAnimation = -1;
+
+	void Awake () {
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		Aiming ();
+//		animator.SetTrigger ("Attack");
+
+
+
+		float h = Input.GetAxis ("Horizontal");				// 入力デバイスの水平軸をhで定義
+		float v = Input.GetAxis ("Vertical");	
+
+		velocity = new Vector3 (0, 0, v);	// 上下のキー入力からZ軸方向の移動量を取得
+		velocity = transform.TransformDirection (velocity);
+
+		if (v > 0) {
+			velocity *= 2;		// 移動速度を掛ける
+		} else if (v < 0) {
+			velocity *= 1;	// 移動速度を掛ける
+		}
+
+		col = GetComponent<CapsuleCollider> ();
+
+		// 上下のキー入力でキャラクターを移動させる
+		transform.localPosition += velocity * Time.fixedDeltaTime;
+
+		// 左右のキー入力でキャラクタをY軸で旋回させる
+		transform.Rotate (0, h * 2, 0);	
+
+		if (Input.GetButton ("Fire1")) {
+			Aiming ();
+			animator.SetTrigger ("Attack");
+			
+		}
+
+
+	}
+
+	public void Aiming () {
+		animator.SetBool ("Squat", false);
+		animator.SetFloat ("Speed", 0f);
+		animator.SetBool("Aiming", true);
+	}
+}
